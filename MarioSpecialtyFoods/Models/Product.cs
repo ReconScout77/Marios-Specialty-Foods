01dd4ac4500ce43d653f89b52b7fcf15fe8c22c8
Robert Murray <alexmurray93@hotmail.com>
2017-10-20 10:52:40 -0700

    Create Models for Product and Review.

---
+++ MarioSpecialtyFoods/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarioSpecialtyFoods.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Cost { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public bool Featured { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
